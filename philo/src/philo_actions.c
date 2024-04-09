b9dd1c6b0674dd23ac877f7dd974b7d3e6174092
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-04-09 19:18:38 +0200

    OMG IT IS SO PRECISE NOW

--- philo/src/philo_actions.c
+++ philo/src/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 14:26:28 by juestrel          #+#    #+#             */
-/*   Updated: 2024/04/09 15:58:03 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/04/09 18:54:39 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ static void	print_philo_status(char *message, long time, t_philo *philo);
 
 void	eat_even(t_philo *philo)
 {
-	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(philo->left_fork);
 	if (print_check(philo) == ALIVE)
 		print_philo_status("has taken a fork\n", get_time() - philo->start_time,
 			philo);
-	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->right_fork);
 	if (print_check(philo) == ALIVE)
 		print_philo_status("has taken a fork\n", get_time() - philo->start_time,
 			philo);
@@ -36,17 +36,17 @@ void	eat_even(t_philo *philo)
 		pthread_mutex_unlock(philo->philos_full_mutex);
 	}
 	best_usleep((long)philo->program->time_to_eat);
-	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
 }
 
 void	eat_odd(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->right_fork);
 	if (print_check(philo) == ALIVE)
 		print_philo_status("has taken a fork\n", get_time() - philo->start_time,
 			philo);
-	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(philo->left_fork);
 	if (print_check(philo) == ALIVE)
 		print_philo_status("has taken a fork\n", get_time() - philo->start_time,
 			philo);
@@ -62,8 +62,8 @@ void	eat_odd(t_philo *philo)
 		pthread_mutex_unlock(philo->philos_full_mutex);
 	}
 	best_usleep((long)philo->program->time_to_eat);
-	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(philo->left_fork);
 }
 
 void	rest(t_philo *philo)
